1d4d18307fbf37e3e65b0d4e12975f676c6ed6e7
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-02-04 21:56:06 +0600

    Encapsulation added

--- Object_oriented_typescript_and_generics/output/main.js
+++ Object_oriented_typescript_and_generics/output/main.js
@@ -17,12 +17,37 @@ class UserInfo {
 }
 const user = new UserInfo('Ibrahim', 27, true);
 console.log(user.displayInfo());
+// => Encapsulation Example 💛💛💛
 class BankAccount {
     constructor(balance, id) {
         this.balance = balance;
         this.id = id;
-        this.balance = balance,
-            this.id = id;
+        this.balance = balance;
+        this.id = id;
+    }
+    deposit(amount) {
+        if (amount <= 0) {
+            throw new Error('Amount Greater Than 0');
+        }
+        this.recordTransaction();
+        this.balance += amount;
+    }
+    recordTransaction() {
+        console.log('Record Amount');
+    }
+    withDraw(amount) {
+        if (amount > this.balance) {
+            throw new Error('Not Available Balance');
+        }
+        this.recordTransaction();
+        this.balance -= amount;
+    }
+    getBalance() {
+        return this.balance;
     }
 }
-const balance;
+const account = new BankAccount(10, 'rahim23');
+console.log(account);
+account.deposit(1001);
+account.withDraw(1000);
+console.log(account.getBalance());
--- Object_oriented_typescript_and_generics/src/main.ts
+++ Object_oriented_typescript_and_generics/src/main.ts
@@ -30,17 +30,43 @@ const user = new UserInfo('Ibrahim', 27, true)
 console.log(user.displayInfo());
 
 
-
-class BankAccount{
+// => Encapsulation Example 💛💛💛
+class BankAccount {
     constructor(
-      private  balance : number,
-      private  id: string
+        private balance : number,
+        private id : string
     ){
-        this.balance = balance,
+        this.balance = balance
         this.id = id
+    }
+    
+    deposit(amount: number): void{
+        if(amount <= 0){
+            throw new Error('Amount Greater Than 0')
+        }
+        this.recordTransaction();
+        this.balance += amount
+    }
+
+    recordTransaction(){
+        console.log('Record Amount');
+    }
+
+    withDraw(amount: number): void{
+        if(amount > this.balance){
+            throw new Error('Not Available Balance')
+        }
+        this.recordTransaction();
+        this.balance -= amount
+    }
+
+    getBalance(){
+        return this.balance;
     }
 }
 
-
-
-const balance
+const account = new BankAccount(10, 'rahim23');
+console.log(account);
+account.deposit(1001)
+account.withDraw(1000)
+console.log(account.getBalance());
